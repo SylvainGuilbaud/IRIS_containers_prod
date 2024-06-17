7693998b9a588fa23a874d194797e9482041386d
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2024-06-17 13:49:13 +0200

    init

--- iris/configure.sh
+++ iris/configure.sh
@@ -3,5 +3,6 @@ cat << EOF | iris session iris
 do ##class(%SYSTEM.CSP).SetConfig("CSPConfigName","$HOSTNAME")
 zn "%sys"
 do ##class(Security.Users).UnExpireUserPasswords("*")
+do ##class(%EnsembleMgr).EnableNamespace("USER")
 halt
 EOF
